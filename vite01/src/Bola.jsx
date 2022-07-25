0a8956d0bdcb9402c7d981006e15f34ced3c32bd
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-07-25 10:23:33 +0200

    vite testing

---
+++ vite01/src/Bola.jsx
@@ -0,0 +1,9 @@
+function Bola(){
+    return (
+        <>
+            <div className="bola">01</div>
+        </>
+    )
+}
+
+export default Bola;
---
+++ vite01/src/main.jsx
@@ -0,0 +1,11 @@
+/* l'arxiu main.jsx està en minúscules perquè no retorna cap element visual JSX */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
