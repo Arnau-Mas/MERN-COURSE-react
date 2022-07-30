ad8ced81d4964c4bb4599f08927a99ab47abe82f
Arnau-Mas <arnau.mava@gmail.com>
2022-07-30 19:32:28 +0200

    refactoring code

--- ejercicios_con_forms/src/Autocompletar.jsx
+++ ejercicios_con_forms/src/Autocompletar.jsx
@@ -3,11 +3,13 @@ import LlistatCiutats from "./LlistatCiutats";
 
 function Autocompletar(){
     const [municipi, setMunicipi] = useState("");
+    const [active, setActive] = useState(false);
     return (
         <div>
             <h3>Autocompletar</h3>
             <input type="text" onChange={(e) => setMunicipi(e.target.value)} value={municipi}></input>
-            <LlistatCiutats nom={municipi}/>
+            <button style={{backgroundColor:"orange", marginLeft:"1rem", padding:"0.6rem"}} onClick={() => setActive(!active)}>⬍ {active ? "Ocultar llista" : "Mostrar llista"}</button>
+            {active && <LlistatCiutats nom={municipi} />}
         </div>
     )
 }
--- ejercicios_con_forms/src/LlistatCiutats.jsx
+++ ejercicios_con_forms/src/LlistatCiutats.jsx
@@ -1,22 +1,12 @@
 import CIUTATS from "./ciutats.json";
 
-function LlistatCiutats({nom}){
-    if(nom){
-        let nomInicial = [...nom];
-        nomInicial[0] = nomInicial[0].toUpperCase();
-        nomInicial = nomInicial.join("");
-        let ciutatsFiltrades = CIUTATS.filter(ciutat => ciutat.municipi.startsWith(nomInicial))
+function LlistatCiutats({nom = ""}){
+        let ciutatsFiltrades = CIUTATS.filter(ciutat => ciutat.municipi.toUpperCase().startsWith(nom.toUpperCase()))
         return (
             <ul>
                 {ciutatsFiltrades.map(ciutat => <li key={ciutat.municipi}>{ciutat.municipi}</li> )}
             </ul>
         )
     }
-    return (
-        <ul>
-            {CIUTATS.map(ciutat => <li key={ciutat.municipi}>{ciutat.municipi}</li> )}
-        </ul>
-    )
-}
 
 export default LlistatCiutats;
