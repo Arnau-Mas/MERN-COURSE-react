bae77ebbc89567b169b2057db788baf7b0b0ddcf
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-08-08 12:31:04 +0200

    update

--- R12_parcs_barcelona_routerContext/src/App.jsx
+++ R12_parcs_barcelona_routerContext/src/App.jsx
@@ -3,10 +3,18 @@ import { NavBar } from './components/NavBar'
 import { Outlet, Link } from "react-router-dom";
 import ComponentTraductor from './components/ComponentTraductor';
 import diccionari from "./components/diccionari.js"
+import { useState } from 'react';
 
 function App() {
+  const [idioma, setIdioma] = useState(0);
+  
+  function traduce(etiqueta){
+    let frases = diccionari[etiqueta]
+    return frases[idioma]
+  }
 
   return (
+    <ComponentTraductor.Provider >
     <div className="App">
       <div className='title'>
         <Link to="/"><h1>Salir.com</h1></Link>
@@ -15,15 +23,14 @@ function App() {
           <button>ES</button>
         </div>
       </div>
-      <ComponentTraductor.Provider >
         <div style={{display:"flex", gap:"4rem"}}>
           <NavBar/>
           <div style={{marginTop:"2rem"}}>
             <Outlet/>
           </div>
         </div>
+      </div>
       </ComponentTraductor.Provider>
-    </div>
   )
 }
 
--- R12_parcs_barcelona_routerContext/src/components/diccionari.js
+++ R12_parcs_barcelona_routerContext/src/components/diccionari.js
@@ -1,5 +1,6 @@
 const diccionario = {
-	estas: ["te encuentras en la página", "et trobes a la pàgina"]
+	homeTitle: ["Els millors Parcs Naturals de Barcelona!", "Los mejores Parques Naturales de Barcelona!"],
+    homeText: ["T'agrada la natura? Has de saber que, encara que visquis a Barcelona oa la seva àrea metropolitana, a prop de la ciutat trobaràs espais naturals que són perfectes per perdre's i gaudir d'un dia a l'aire lliure. En aquest llistat de Salir.com hem recopilat els millors parcs naturals a prop de Barcelona i el més llunyà de tots és a menys d'1 hora i mitja, per tant podràs fer una escapada tranquil·lament d'un dia. Tots aquests espais són ideals per descobrir al costat dels teus amics o família i t'ofereixen bells racons on la natura mostra la millor faceta. Descobreix-los!", "¿Te gusta la naturaleza? Debes saber que, aunque vivas en Barcelona o en su área metropolitana, cerca de la ciudad encontrarás espacios naturales que son perfectos para perderse y disfrutar de un día al aire libre. En este listado de Salir.com hemos recopilado los mejores parques naturales cerca de Barcelona y el más lejano de todos está a menos de 1 hora y media, por tanto, podrás hacer una escapada de un día tranquilamente. Todos estos espacios son ideales para descubrir junto a tus amigos o familia y te ofrecen bellos rincones donde la naturaleza muestra su mejor faceta. ¡Descúbrelos!"]
 }
 
 export default diccionario;
