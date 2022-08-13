5a44dd741ed1d455a6149a699afc625d5827e1c2
Arnau-Mas <arnau.mava@gmail.com>
2022-08-13 20:44:36 +0200

    update

--- R23_Memory/src/App.jsx
+++ R23_Memory/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [active, setActive] = useState([]);
   const [numbers, setNumber] = useState([])
   const [twoNumbers, setTwoNumbers] = useState([])
-  
+  /* Valorar si crear un array de valors que anirien enllaçats a cada un dels components */
   useEffect(() => {
     let activeArray= [];
     let numbersArray = [];
